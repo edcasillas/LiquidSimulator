08bde6144d1644c7b89ee86a898b00661ebef01c
Ed <eduardo_casillas@hotmail.com>
2023-01-20 11:00:28 -0800

    Removed unnecessary offset parameter in the GridLineRenderer by using local positions instead

--- Runtime/Scripts/GridLinesRenderer.cs
+++ Runtime/Scripts/GridLinesRenderer.cs
@@ -7,8 +7,8 @@ namespace Liquids2D {
 		GridLine[] VerticalLines { get; }
 		Color Color { get; }
 
-		void CreateGridLines(Vector2 offset, float CellSize, float LineWidth);
-		void RenderGridLines(Vector2 offset, float CellSize, float LineWidth);
+		void CreateGridLines(float CellSize, float LineWidth);
+		void RenderGridLines(float CellSize, float LineWidth);
 	}
 
 	public class GridLinesRenderer : MonoBehaviour, IGridLineRenderer {
@@ -20,45 +20,45 @@ namespace Liquids2D {
 		public GridLine[] VerticalLines { get; private set; }
 		public Color Color => color;
 
-		public void CreateGridLines(Vector2 offset, float CellSize, float LineWidth) {
-			var gridLineContainer = new GameObject ("GridLines");
-			gridLineContainer.transform.parent = this.transform;
+		public void CreateGridLines(float CellSize, float LineWidth) {
+			var gridLineContainer = new GameObject ("GridLines").transform;
+			gridLineContainer.parent = transform;
 
 			// vertical grid lines
 			VerticalLines = new GridLine[Grid.Width + 1];
 			for (int x = 0; x < Grid.Width + 1; x++) {
-				var line = Instantiate(gridLinePrefab);
-				float xpos = offset.x + (CellSize * x) + (LineWidth * x);
+				var line = Instantiate(gridLinePrefab, gridLineContainer);
+				float xpos = (CellSize * x) + (LineWidth * x);
 				line.Set (color,
-					new Vector2 (xpos, offset.y),
+					new Vector2 (xpos, 0),
 					new Vector2 (LineWidth, (Grid.Height*CellSize) + LineWidth * Grid.Height + LineWidth));
-				line.transform.parent = gridLineContainer.transform;
 				VerticalLines [x] = line;
 			}
 
 			// horizontal grid lines
 			HorizontalLines = new GridLine[Grid.Height + 1];
 			for (int y = 0; y < Grid.Height + 1; y++) {
-				var line = Instantiate(gridLinePrefab);
-				float ypos = offset.y - (CellSize * y) - (LineWidth * y);
-				line.Set (color, new Vector2 (offset.x, ypos), new Vector2 ((Grid.Width*CellSize) + LineWidth * Grid.Width + LineWidth, LineWidth));
-				line.transform.parent = gridLineContainer.transform;
+				var line = Instantiate(gridLinePrefab, gridLineContainer);
+				float ypos = (CellSize * y) - (LineWidth * y);
+				line.Set (color,
+					new Vector2 (0, ypos),
+					new Vector2 ((Grid.Width*CellSize) + LineWidth * Grid.Width + LineWidth, LineWidth));
 				HorizontalLines [y] = line;
 			}
 		}
 
-		public void RenderGridLines(Vector2 offset, float CellSize, float LineWidth) {
+		public void RenderGridLines(float CellSize, float LineWidth) {
 			if (!enable) return;
 			// vertical grid lines
 			for (int x = 0; x < Grid.Width + 1; x++) {
-				float xpos = offset.x + (CellSize * x) + (LineWidth * x);
-				VerticalLines [x].Set (color, new Vector2 (xpos, offset.y), new Vector2 (LineWidth, (Grid.Height*CellSize) + LineWidth * Grid.Height + LineWidth));
+				float xpos = (CellSize * x) + (LineWidth * x);
+				VerticalLines [x].Set (color, new Vector2 (xpos, 0), new Vector2 (LineWidth, (Grid.Height*CellSize) + LineWidth * Grid.Height + LineWidth));
 			}
 
 			// horizontal grid lines
 			for (int y = 0; y < Grid.Height + 1; y++) {
-				float ypos = offset.y - (CellSize * y) - (LineWidth * y);
-				HorizontalLines [y] .Set (color, new Vector2 (offset.x, ypos), new Vector2 ((Grid.Width*CellSize) + LineWidth * Grid.Width + LineWidth, LineWidth));
+				float ypos = (CellSize * y) - (LineWidth * y);
+				HorizontalLines [y] .Set (color, new Vector2 (0, ypos), new Vector2 ((Grid.Width*CellSize) + LineWidth * Grid.Width + LineWidth, LineWidth));
 			}
 		}
 	}
